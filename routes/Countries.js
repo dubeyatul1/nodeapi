6a18aaff05dd64614f90f6fe780ca5be03fe938c
ss251326 <ss251326@ncr.com>
2018-11-08 12:56:06 +0530

    Changes countries file name

--- routes/Countries.js
+++ routes/Countries.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const Countries = require('../model/Country');
 
 /* GET users listing. */
